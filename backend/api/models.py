6b3753e41032cd8cbe2616d90bba0609db01edc6
MohammadAshour <36931318+MohammadAshour@users.noreply.github.com>
2026-04-11 14:55:05 +0200

    update models.py to create tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Condition(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Medicine(models.Model):
+    name_ar = models.CharField(max_length=200)
+    name_en = models.CharField(max_length=200, blank=True)
+    shelf_life_months = models.PositiveIntegerField()
+    shelf_life_after_opening_months = models.PositiveIntegerField()
+    conditions = models.ManyToManyField(Condition)
+
+    def __str__(self):
+        return self.name_ar
+
+class MedicineInstance(models.Model):
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    production_date = models.DateField()
+    open_date = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.medicine.name_ar} - {self.location}"
