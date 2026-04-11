60417f7826e5befc8fbebf3f57d2c5e0803db1af
MohammadAshour <36931318+MohammadAshour@users.noreply.github.com>
2026-04-11 20:43:09 +0000

    use environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+5st6mkc48ozjcfn*z6u=e$9+$a1x_nve4lfj^k%-i!8fatb6!"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -79,11 +78,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "railway",
-        "USER": "root",
-        "PASSWORD": "SjTViSMZZsuZWthrWRGMyarrzowfajaE",
-        "HOST": "tramway.proxy.rlwy.net",
-        "PORT": "44097",
+        "NAME": os.environ.get('DB_NAME', 'railway'),
+        "USER": os.environ.get('DB_USER', 'root'),
+        "PASSWORD": os.environ.get('DB_PASSWORD', ''),
+        "HOST": os.environ.get('DB_HOST', 'tramway.proxy.rlwy.net'),
+        "PORT": os.environ.get('DB_PORT', '44097'),
     }
 }
 
